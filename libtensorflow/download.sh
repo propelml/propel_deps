4a0dfa366e6277b81df2b0820d491780590b9f0b
Ryan Dahl <ry@tinyclouds.org>
2017-12-19 03:32:15 -0500

    Upgrade libtensorflow packages to latest nightly.

---
+++ libtensorflow/download.sh
@@ -0,0 +1,4 @@
+
+TF_TYPE="cpu" # Change to "gpu" for GPU support
+OS="linux" # Change to "darwin" for Mac OS
+curl -L -O "https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-${TF_TYPE}-${OS}-x86_64-1.4.0.tar.gz" 
