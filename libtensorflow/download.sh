3df27c480c414dbcf93962f8ad4925d53ebbb357
Bert Belder <bertbelder@gmail.com>
2018-02-02 14:27:32 -0800

    Upgrade to libtensorflow v1.5.0

--- libtensorflow/download.sh
+++ libtensorflow/download.sh
@@ -1,4 +1,4 @@
 
 TF_TYPE="cpu" # Change to "gpu" for GPU support
 OS="linux" # Change to "darwin" for Mac OS
-curl -L -O "https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-${TF_TYPE}-${OS}-x86_64-1.4.0.tar.gz" 
+curl -L -O "https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-${TF_TYPE}-${OS}-x86_64-1.5.0.tar.gz"
